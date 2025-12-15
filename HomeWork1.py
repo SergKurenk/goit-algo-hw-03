fa7fc963d08651fa3a56d89c16128ed844965376
Artem <t369@ukr.net>
2025-12-15 14:04:31 +0200

    HomeWork second edition

--- HomeWork1.py
+++ HomeWork1.py
@@ -28,12 +28,17 @@ def normalize_phone(phone_number):
     return match
 
 p_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
     "    +38(050)123-32-34",
     "     0503451234",
     "(050)8889900",
     "38050-111-22-22",
-    "38050 111 22 11   "]
+    "38050 111 22 11   ",
+]
 
-print([normalize_phone(x) for x in p_numbers])
+print("Нормалізовані номери телефонів для SMS-розсилки:", [normalize_phone(x) for x in p_numbers])
 print (get_days_from_today("2025_12-25"))
 print(get_numbers_ticket(1, 100, quantity=10))
